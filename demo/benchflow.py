f2a891f8d465aa27ac02c9f8b53305efa8bf2e15
simonedavico <simonedavico@gmail.com>
2016-01-10 15:27:30 +0100

    Added help support
    The client now supports the `—help` option.
    
    Running `python benchflow.py —help` will show a generic help section
    listing all commands, together with a short description.
    
    Running `python benchflow.py <command> —help` will show a more detailed
    help section regarding the usage of a given `<command>`.
    
    Also, added `.gitignore` file to ignore `virtualenv`s.

--- demo/benchflow.py
+++ demo/benchflow.py
@@ -9,26 +9,34 @@ def expManager():
 	pass	
 
 @expManager.command()
-@click.argument("benchmark", type=click.Path(exists=True))
+@click.argument("benchmark", type=click.Path(exists=True), 
+				metavar="<benchmark>")
 def deploy(benchmark):
-	"""Deploys a BenchFlow benchmark"""
+	"""Deploys a BenchFlow <benchmark> zip archive."""
 	filename = click.format_filename(benchmark)
 	file = { 'file': open(filename, 'rb') }
+	click.echo("Deploying benchmark...")
 	r = requests.post(expManagerAddress + "/faban/deploy", files=file)
 	click.echo(r.json())
 
 @expManager.command()
-@click.argument("benchmarkshortname")
+@click.argument("benchmarkshortname", metavar="<shortName>")
+@click.argument("configuration", type=click.Path(exists=True),
+				metavar="<configuration>")
 #@click.argument("configfile", type=click.Path(exists=True))
-def run(benchmarkshortname):
-	"""Runs a BenchFlow benchmark"""
-	r = requests.post(expManagerAddress + "/faban/run/" + benchmarkshortname)
+def run(benchmarkshortname, configuration):
+	"""Runs a BenchFlow benchmark 
+	given its <shortName> and a <configuration>"""
+	filename = click.format_filename(configuration)
+	body = { 'config' : open(filename, 'rb') }
+	address = expManagerAddress + "/faban/run/" + benchmarkshortname
+	r = requests.post(address, files=body)
 	click.echo(r.json())
 
 @expManager.command()
-@click.argument("runid")
+@click.argument("runid", metavar="<runId>")
 def status(runid):
-	"""Returns the status of a benchmark run"""
+	"""Returns the status of a benchmark run, given its <runId>"""
 	r = requests.get(expManagerAddress + "/faban/status/" + runid)
 	click.echo(r.json())		
 
@@ -37,10 +45,10 @@ def cassandra(args):
 	pass
 
 @cassandra.command()
-@click.argument("host")
-@click.argument("port")
+@click.argument("host", metavar="<host>")
+@click.argument("port", metavar="<port>")
 def clsqh(host, port):
-	"""Starts clsqh"""
+	"""Starts clsqh on specified <host> and <port>"""
 	cmd = list(("clsqh", host, port))
 	click.echo(cmd)
 	subprocess.run(cmd)
